15f700d8356d6967ee28f1845f6ca57866ec91f0
git-mal <pridemoremullins@gmail.com>
2025-03-16 22:36:43 -0400

    built out player class, display, and movement

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import pygame
+from circleshape import *
+from player import *
 from constants import *
 
 
 
 def main():
     pygame.init()
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable,drawable)
+
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    
+    
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        for object in drawable:
+            object.draw(screen)
+        #drawable.draw(screen)
+        updateable.update(dt)
         pygame.display.flip()
+       
+        dt = clock.tick(60) / 1000
 
 
 
@@ -35,11 +56,7 @@ def main():
    
    
    
-    '''
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    '''
+   
 
 if __name__ == "__main__":
     main()
